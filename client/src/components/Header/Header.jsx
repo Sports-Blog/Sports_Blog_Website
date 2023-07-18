e8f317d2b0feac2a2938d2392647b5feed408b6e
DPapachristou <d.k.papachristou@gmail.com>
2023-07-18 18:16:33 +0300

    Final Update

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
         <div>
         <img 
         className="headerImg" 
-        src="https://store-images.s-microsoft.com/image/apps.2127.14621801316397473.3c7c0657-fe34-4727-8700-af9b251375de.193acee6-86b5-42a2-82f7-6ab2d5acea1a?mode=scale&q=90&h=1080&w=1920"
+        src="https://djcs-prod.s3.amazonaws.com/public/blogs/deloitte/blogs.dir/8/files/2023/01/Live-Sports-IMAGE.png"
         alt="" />
         </div>
     </div>
